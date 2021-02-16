e96ee1e9f5c25bc9ab75101a3640980531ce9761
Wifey <wifey@Adrians-MacBook-Air.local>
2021-02-16 20:40:43 +0000

    - Finished links to individual character pages using id - Basic CSS styling for easier time seeing rendered data

--- videogame-map/src/components/Character.jsx
+++ videogame-map/src/components/Character.jsx
@@ -2,8 +2,7 @@ import { useParams } from 'react-router-dom';
 
 function Character(props) {
   const params = useParams(0);
-  const charType = props.characters.find((character) => character.charName === params.name);
-  console.log(charType)
+  const charType = props.characters.find((character) => character.id === params.id);
 
   if (!charType) {
     return <h4>Finding character...</h4>;
@@ -11,14 +10,15 @@ function Character(props) {
 
   return (
     
-    <div>
+    <div className="charDetails">
       <img src={charType.fields.charImage} />
       <h1>{charType.fields.charName}</h1>
       <h3>{charType.fields.origin}</h3>
       <h3>{charType.fields.game}</h3>
       <h3>{charType.fields.genre}</h3>
       <h3>{charType.fields.pocLead ? "POC Protagonist" : null}</h3>
-    
+      <h3>{charType.fields.femaleLead ? "Female Protagonist" : null}</h3>
+      <h3>{charType.fields.lgbtqaLead ? "Queer Protagonist" : null}</h3>
     </div>
   )
 }
