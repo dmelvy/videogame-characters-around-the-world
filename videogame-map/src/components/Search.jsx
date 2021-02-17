2eb675308a18daa89df36282b5e502d78f468fdf
Wifey <wifey@Adrians-MacBook-Air.local>
2021-02-17 13:15:47 +0000

    - Added grid styling to better manage data rendering - Added filter form - Populated fform with search conditions - Added fixed background image

--- videogame-map/src/components/Search.jsx
+++ videogame-map/src/components/Search.jsx
@@ -1,9 +1,17 @@
 function Search(props) {
   return (
-    <div>
+    <div className="search-filter">
       <form>
-        <input type="search"></input>
+        <h3>Filter by character or game</h3>
+        <label htmlFor="character-keyword">Character Name</label>
+        <input type="search" placeholder="Try typing a character name."></input>
+        <label htmlFor="videogame-keyword">Videogame Title</label>
+        <input type="search" placeholder="Type a videogame title."></input>
+        <label htmlFor="queerLead">LGBTQA Protagonist</label>
         <input type="checkbox"></input>
+        <label htmlFor="pocLead">Person of Color (POC) Protagonist</label>
+        <input type="checkbox"></input>
+        <label htmlFor="femaleLead">Female Protagonist</label>
         <input type="checkbox"></input>
 
         <button type="submit">Find Characters</button>
